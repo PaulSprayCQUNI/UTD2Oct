1685f6e5dc05e5edc31c84e6e43a04e79357b964
Paul S <paul.spray@cqumail.com>
2019-10-17 14:55:11 +1000

    Oct 17 254
    Thursday push

--- TDProto/TDProto1908.1/Assets/Scripts/CredsUI.cs
+++ TDProto/TDProto1908.1/Assets/Scripts/CredsUI.cs
@@ -1,27 +1,14 @@
-﻿using System.Collections;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 using UnityEngine;
-using UnityEngine.EventSystems;
+
 
 public class CredsUI : MonoBehaviour
 {
 	public Text credsText;
-    public Color lowCredColor;
-
-    private BuildManagement buildManagement;
 
 
     void Update()
 	{
 		credsText.text = "Creds: ($)" + PlayerStats.Creds.ToString();
-
-        // to do -- NullReferenceException: Object reference not set to an instance of an object
-        // CredsUI.Update()(at Assets / Scripts / CredsUI.cs:19)
-
-        if (!buildManagement.lowCreds)
-        {
-            credsText.text = string.Format("Creds: ($) <color=lowCredColor>{0}</color" + PlayerStats.Creds.ToString());
-
-        }
 	}
 }
