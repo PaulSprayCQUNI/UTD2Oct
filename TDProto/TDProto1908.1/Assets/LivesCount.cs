73cb42719b6713361a5d2c7fed39c9aadeef8653
Paul S <paul.spray@cqumail.com>
2019-10-18 10:27:08 +1000

    18October 1027
    take a break push

---
+++ TDProto/TDProto1908.1/Assets/LivesCount.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class LivesCount : MonoBehaviour
+{
+	public Text LivesText;
+    void Update()
+    {
+	    LivesText.text = PlayerStats.Lives.ToString() + " LIVES:";
+    }
+}
--- TDProto/TDProto1908.1/Assets/Scripts/PlayerStats.cs
+++ TDProto/TDProto1908.1/Assets/Scripts/PlayerStats.cs
@@ -8,10 +8,12 @@ public class PlayerStats : MonoBehaviour
     public static int Creds;
     public int startCreds = 1000;
 
-    private
+    public static int Lives;
+	public int startLives = 10;
 
     void Start()
     {
         Creds = startCreds;
+		Lives = startLives;
     }
 }
