3183e8747097c9e8edd981220eaa61c16bc3216c
Paul S <paul.spray@cqumail.com>
2019-10-26 15:00:27 +1000

    26Oct 1500
    Saturday afternoon commit

---
+++ TDProto/TDProto1908.1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+	public string levelToLoad = "FirstLevel";
+
+	public void Play()
+	{
+		SceneManager.LoadScene(levelToLoad);
+	}
+
+   public void Quit()
+   {
+	   Debug.Log("Quit/Exit program function called");
+	   Application.Quit();
+   }
+}
